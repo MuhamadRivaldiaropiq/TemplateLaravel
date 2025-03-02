7d52ff7f21bcfa381dd2bab20fae3187d0b84f29
MuhamadRivaldiaropiq <116324069+MuhamadRivaldiaropiq@users.noreply.github.com>
2025-03-02 07:32:22 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,18 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+                User::insert([
+            [
+                'name' => 'User',
+                'email' => 'User@gmail.com',
+                'password' => Hash::make('12345678'),
+            ]
         ]);
+
     }
 }
